7d2ec2de31fc8dc2fca90c836efdc3f7a59a0557
namesamename <mary022689@naver.com>
2022-04-04 18:11:54 +0900

    second

---
+++ practice/practice6.py
@@ -0,0 +1,11 @@
+jieun='990226-2123456'
+
+print('성별: '+jieun[7])
+print('생년: '+jieun[0:2]) #0부터 2 이전까지
+print('월: '+jieun[2:4])
+print('일: '+jieun[4:6])
+
+print('생년월일: '+jieun[:6])
+print('뒤 7자리: '+jieun[7:])
+
+print('(뒤에서부터 센) 뒤 7자리: '+jieun[-7:])
